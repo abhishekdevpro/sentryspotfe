71407cb5d606469bc67d0cf0d1d0f2859d11e4ec
Rishi kashyap <kashyap1strishi@gmail.com>
2025-08-12 11:35:49 +0530

    bug-fixed-3

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -1,26 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { IoBagHandleOutline, IoPlay } from "react-icons/io5";
-import { CiLocationOn } from "react-icons/ci";
-import {
-  MdOutlineHealthAndSafety,
-  MdPhoto,
-  MdLocalCafe,
-  MdEdit,
-} from "react-icons/md";
-import {
-  FaHospital,
-  FaHeart,
-  FaCalendarAlt,
-  FaLinkedin,
-  FaTwitter,
-  FaFacebook,
-  FaGlobe,
-} from "react-icons/fa";
-import { TbTargetArrow } from "react-icons/tb";
-import { FaCarBurst } from "react-icons/fa6";
-import DOMPurify from "dompurify";
 import InsideCognizant from "./InsideCognizant ";
 import { Constant } from "@/utils/constant/constant";
 import LeadershipTeam from "./LeaderShipTeams";
@@ -38,7 +18,6 @@ import AboutSection from "./AboutSection.jsx"
 const ShowcaseComponent = ({ companyId }) => {
   const [companyData, setCompanyData] = useState(null);
   const [selectedImages, setSelectedImages] = useState([]);
-  const [openImageGallery, setOpenImageGallery] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [formData, setFormData] = useState({
     title: "",
@@ -55,7 +34,6 @@ const ShowcaseComponent = ({ companyId }) => {
   const navigate = useNavigate();
   const { userInfo } = useSelector((state) => state.auth);
 
-  // console.log(userInfo,"user hu main");
   useEffect(() => {
     const fetchCompanyData = async () => {
       try {
@@ -88,121 +66,6 @@ const ShowcaseComponent = ({ companyId }) => {
     fetchCompanyData();
   }, []);
 
-  const handleEditClick = () => {
-    setIsPopupOpen(true);
-  };
-
-  const handleClosePopup = () => {
-    setIsPopupOpen(false);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({ ...prevState, [name]: value }));
-  };
-
-  const handleImageChange = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      image: URL.createObjectURL(e.target.files[0]),
-    }));
-  };
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const token = localStorage.getItem(Constant.USER_TOKEN);
-      const response = await axios.put(
-        "https://api.sentryspot.co.uk/api/employeer/company",
-        formData,
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-      console.log("Update Successful", response.data);
-      setIsPopupOpen(false);
-    } catch (error) {
-      console.error("Error updating data", error);
-    }
-  };
-
-  const handleImageChange2 = (e) => {
-    const files = Array.from(e.target.files);
-    if (files.length > 3) {
-      alert("You can upload a maximum of 3 images.");
-    } else {
-      setSelectedImages(files);
-    }
-  };
-
-  const handleSave2 = async () => {
-    if (selectedImages.length > 3) {
-      alert("Please ensure only 3 images are selected.");
-      return;
-    }
-
-    const formData = new FormData();
-
-    formData.append("title", companyData.title);
-    formData.append("about", companyData.about);
-    selectedImages.forEach((image) =>
-      formData.append("about_images_upload", image)
-    );
-
-    try {
-      const response = await axios.patch(
-        "https://api.sentryspot.co.uk/api/employeer/company-about",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: token,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        toast.success("Content updated successfully!");
-        setIsPopupOpen(false);
-      } else {
-        alert("Failed to update content. Please try again.");
-      }
-    } catch (error) {
-      console.error("Error updating content:", error);
-      toast.error("An error occurred. Please try again.");
-    }
-  };
-  const handleSave3 = async () => {
-    const formData = new FormData();
-
-    // formData.append("title", companyData.title)
-    formData.append("summery", companyData.summery);
-
-    try {
-      const response = await axios.patch(
-        "https://api.sentryspot.co.uk/api/employeer/company-about",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: token,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        toast.success("Content updated successfully!");
-        setIsPopupOpen(false);
-      } else {
-        alert("Failed to update content. Please try again.");
-      }
-    } catch (error) {
-      console.error("Error updating content:", error);
-      toast.error("An error occurred. Please try again.");
-    }
-  };
 
   if (!companyData) {
     return (
@@ -237,43 +100,10 @@ const ShowcaseComponent = ({ companyId }) => {
 
   return (
     <>
-      {/* <AboutSection companyData={companyData} userInfo={userInfo} /> */}
-
-      {/* <div className="app-gradient-bg w-full">
-        <WhyChooseUsSection companyData={companyData} userInfo={userInfo} />
-
-      <section id="inside-cognizant">
-        <InsideCognizant companyData={companyData} userInfo={userInfo} />
-      </section>
-      <CompanyBenefits companyData={companyData} />
-      <LeadershipTeam companyId={companyId} />
-      <JobListings companyData={companyData} userInfo={userInfo} />
-      <SocialFooter companyData={companyData} />
-      </div> */}
-
-      {/* <div className="app-gradient-bg w-full">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 space-y-16">
-          <AboutSection companyData={companyData} userInfo={userInfo}/>
-          <WhyChooseUsSection companyData={companyData} userInfo={userInfo} />
-
-          <section id="inside-cognizant">
-            <InsideCognizant companyData={companyData} userInfo={userInfo} />
-          </section>
-
-          <CompanyBenefits companyData={companyData} />
-
-          <LeadershipTeam companyId={companyId} />
-
-          <JobListings companyData={companyData} userInfo={userInfo} />
-
-          <SocialFooter companyData={companyData} />
-        </div>
-      </div> */}
-
       <div className="app-gradient-bg w-full">
         <div className="max-w-7xl mx-auto px-2 space-y-4 py-4">
           {/* About Section */}
-          <AboutSection companyData={companyData} userInfo={userInfo} />
+          {/* <AboutSection companyData={companyData} userInfo={userInfo} /> */}
 
           {/* Navigation Bar */}
           <NavigationBar />
